6877156c1a6a01e1fdb18d5823a3dbb2d01e32a6
Marcos phelipe <Marcosphelipesilva2@gmail.com>
2026-02-12 23:13:18 -0300

    Add files via upload

--- sistema_de_notas.py
+++ sistema_de_notas.py
@@ -1,7 +1,37 @@
 nota1 = float(input("digite a primeira nota: "))
+
+if (nota1 <= 0 or nota1 > 10 ):
+    print("apenas numeros de 0-10")
+    input("enter para sair")
+    exit()
+
+
+
+
 nota2 = float(input("digite a segunda nota: "))
+
+if (nota2 <= 0 or nota2 >10 ):
+    print("apenas numeros de 0-10")
+    input("aperteenter para sair")
+    exit()
+
+
 nota3 = float(input("digite a terceira nota: "))
+
+if (nota3 <= 0 or nota3 >10 ):
+    print("apenas numeros de 0-10")
+    input("enter para sair")
+    exit()
+
+
+
 media = ((nota1+nota2+nota3) /3)
+
+
+
+    
+
+
 
 if (media >= 6):
     print("passou de ano! ")
